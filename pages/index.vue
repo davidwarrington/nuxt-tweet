7fa1dafae9d1fd10dd8f4ea6126e6b5827257381
davidwarrington <davidmwarrington+github@gmail.com>
2019-03-12 22:29:11 +0000

    Converts index page to list of tweets

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,19 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-tweet
-      </h1>
-      <h2 class="subtitle">
-        A demo that integrates a Twitter feed into a static Nuxt site
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-  </section>
+	<ul>
+		<li v-for="tweet in tweets" :key="tweet.id">
+			{{ tweet.text }}
+		</li>
+	</ul>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import tweets from '@/tweets/tweets'
 
 export default {
-  components: {
-    Logo
-  }
+	data() {
+		return {
+			tweets
+		}
+	}
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
